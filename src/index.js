1409dffec12fc0fbb83bf08b18808a7acd101a02
lokki931 <taurusr2d2@gmail.com>
2022-09-06 19:43:42 +0300

    add localStorage/redux-persist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './store'
-import { Provider } from 'react-redux'
+import { store, persistor } from './store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -10,7 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+
     </Provider>
   </React.StrictMode>
 );
